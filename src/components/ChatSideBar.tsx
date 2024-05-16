ed6d4f3723795627abaeac27fe2ca4a4bc00af38
mulutu <mulutujackson@gmail.com>
2024-05-16 23:55:31 +0300

    First Commit

--- src/components/ChatSideBar.tsx
+++ src/components/ChatSideBar.tsx
@@ -18,7 +18,7 @@ const ChatSideBar = ({ chats, chatId, isPro }: Props) => {
   const [loading, setLoading] = React.useState(false);
 
   return (
-    <div className="w-full overflow-hidden max-h-full soff p-4 text-gray-200 bg-gray-900">
+    <div className="w-full overflow-hidden h-screen soff p-4 text-gray-200 bg-gray-900">
       <Link href="/">
         <Button className="w-full border-dashed border-white border">
           <PlusCircle className="mr-2 w-4 h-4" />
