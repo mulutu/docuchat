9e9bc526c820fd1473df23d10eb3c530a400ec9a
mulutu <mulutujackson@gmail.com>
2024-05-18 17:53:17 +0300

    First Commit

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -30,28 +30,36 @@ export async function POST(req: Request) {
     if (!session?.metadata?.userId) {
       return new NextResponse("no userid", { status: 400 });
     }
-    await db.insert(userSubscriptions).values({
+    const { data, error } = await db.from('userSubscriptions').insert({
       userId: session.metadata.userId,
       stripeSubscriptionId: subscription.id,
       stripeCustomerId: subscription.customer as string,
       stripePriceId: subscription.items.data[0].price.id,
       stripeCurrentPeriodEnd: new Date(subscription.current_period_end * 1000),
     });
+
+    if (error) {
+      console.log("Error creating subscription. ", error)
+    }
   }
 
   if (event.type === "invoice.payment_succeeded") {
     const subscription = await stripe.subscriptions.retrieve(
       session.subscription as string
     );
-    await db
-      .update(userSubscriptions)
-      .set({
+    const { data, error } = await db
+      .from('userSubscriptions')
+      .update({
         stripePriceId: subscription.items.data[0].price.id,
         stripeCurrentPeriodEnd: new Date(
           subscription.current_period_end * 1000
         ),
       })
-      .where(eq(userSubscriptions.stripeSubscriptionId, subscription.id));
+      .eq('stripeSubscriptionId', subscription.id);
+
+    if (error) {
+      console.log("Error updating subscription. ", error)
+    }
   }
 
   return new NextResponse(null, { status: 200 });
